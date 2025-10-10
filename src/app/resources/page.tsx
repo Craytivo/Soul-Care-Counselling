25e2a69e55003524a9662ba51c0bd63a6e848387
Justice Mubayiwa <j.mubayiwa@gmail.com>
2025-10-10 12:30:47 -0600

    Fix: Move 'use client' directive to top of resources page for Next.js build compatibility

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -1,6 +1,6 @@
 
-export const dynamic = 'force-dynamic'
 'use client'
+export const dynamic = 'force-dynamic'
 
 import { useState, useEffect } from 'react'
 import { getResources, getFeaturedResources } from '@/lib/sanity-queries'
